b7c80d8fe3a0640e3aad663b96c840cc235dd321
suchipanda <panda.suchi90@gmail.com>
2020-01-04 17:20:49 +0200

    port changed

--- bookmanagement-client/src/app/service/book.service.ts
+++ bookmanagement-client/src/app/service/book.service.ts
@@ -13,26 +13,26 @@ export class BookService {
 constructor(private http:HttpClient) { }
 
 public getAllBook():Observable<any>{  
-  return this.http.get("http://localhost:8080/api/getBoooks");
+  return this.http.get("http://localhost:8085/api/getBoooks");
 }
 public addBook(book:Book, id:number):Observable<any>{
   console.log(id);
-  return this.http.post("http://localhost:8080/api/addbook/"+id,book);
+  return this.http.post("http://localhost:8085/api/addbook/"+id,book);
 }
 
 public getBookById(id:number):Observable<any>{
-  return this.http.get("http://localhost:8080/api/getbook/"+id);
+  return this.http.get("http://localhost:8085/api/getbook/"+id);
 }
 
 public getAllLibrary():Observable<any> {
-  return this.http.get("http://localhost:8080/getAllLibrary");
+  return this.http.get("http://localhost:8085/getAllLibrary");
 
 }
 public deleteBook(id:number):Observable<any>{
-  return this.http.delete("http://localhost:8080/api/deleteBook/"+id, { responseType: 'text' });
+  return this.http.delete("http://localhost:8085/api/deleteBook/"+id, { responseType: 'text' });
 }
 public updateBook(id:number, value:any):Observable<any>{
-  return this.http.post("http://localhost:8080/api/updateBook/"+id, value);
+  return this.http.post("http://localhost:8085/api/updateBook/"+id, value);
 }
 }
 
