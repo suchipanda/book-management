cdf9de237ce82d7bdb9fe8828f914e664310ba0b
suchipanda <panda.suchi90@gmail.com>
2020-01-06 13:56:43 +0200

    extends servlet initializer

--- src/main/java/za/co/africa/bookmanagement/BookmanagementApplication.java
+++ src/main/java/za/co/africa/bookmanagement/BookmanagementApplication.java
@@ -2,12 +2,13 @@ package za.co.africa.bookmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class BookmanagementApplication {
+public class BookmanagementApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookmanagementApplication.class, args);
--- src/main/java/za/co/africa/bookmanagement/controller/BookController.java
+++ src/main/java/za/co/africa/bookmanagement/controller/BookController.java
@@ -1,8 +1,6 @@
 package za.co.africa.bookmanagement.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 import za.co.africa.bookmanagement.model.Book;
 import za.co.africa.bookmanagement.model.Library;
 import za.co.africa.bookmanagement.service.BookService;
